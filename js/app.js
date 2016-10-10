9e9423d66bfbe2fcbf8f76fff3c0e59472034326
Neto <RONO@gft.com>
2016-10-10 19:19:44 -0300

    oioi

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
 function habilitaCidade() {
 	$("#selCidade").removeClass('hidden');
+	$("#img01").hide();
+	$("#img02").show();
 	
   }
 
-  function habilitaEncontrar() {
+function habilitaEncontrar() {
 	$("#btnEncontrar").removeClass('hidden');
+	$("#img02").hide();
+	$("#img03").show();
 	
   }
 
+function paginaPedido() {
+	$("#idCarousel").addClass('hidden');
+	$("#idPedidos").removeClass('hidden');
+	$("#img03").hide();
+	
+  }
+
+function reloadPagina() {
+	location.reload();
+}
