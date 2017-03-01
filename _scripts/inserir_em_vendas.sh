3f27eaddc39b1e70c8ac6f1b825b41dd029ad183
Neto <rodolfotiago@gmail.com>
2017-03-01 13:39:03 -0300

    imoveis e oportunidade

--- _scripts/inserir_em_vendas.sh
+++ _scripts/inserir_em_vendas.sh
@@ -11,9 +11,6 @@ metro=$(cat $info | grep metro | cut -d':' -f2)
 
 refnumber=$(cat $info | grep ref | cut -d':' -f2 | grep -o [0-9][0-9][0-9])
 
-n=`expr $refnumber % 2`
-if [ $n -eq 0 ]; then
-
 #########
 sed -i "/24062017i/a \
 <div class="col-md-6 about-grid about-one">\n \
@@ -54,46 +51,3 @@ sed -i "/24062017i/a \
 
 sed -i "${inserir}d" $vendas
 #########
-
-
-else
-
-#########
-sed -i "/24062017i/a \
-<!-- start other grid -->\n \
-<div class=\"about-grids\">\n \
-<div class=\"col-md-6 about-grid about-one\">\n \
-<div class=\"about-grid-left\">\n \
-<!-- -->\n \
-<div class=\"w3l_services_grid\">\n \
-<div class=\"hover15 column\">\n \
-<div>\n \
-<a href=\"ref009.html\">\n \
-<figure>\n \
-<img src=\"../images/REF009/001.jpg\" alt=\" \" class=\"img-responsive\" />\n \
-<h5 class=\"cima\"><span class=\"refspan\">REF009</span></h5>\n \
-<h5 class=\"local\"><span class=\"refspan\">Porta do Sol - Mairinque</span></h5>\n \
-</figure>\n \
-</a>\n \
-</div>\n \
-</div>\n \
-<h4><a href=\"ref009.html\">R$ 800.000,00</a></h4>\n \
-<ul>\n \
-<li class=\"square\">2.000 m²</li>\n \
-<li class=\"bath\">1</li>\n \
-<li class=\"bed\">1</li>\n \
-</ul>\n \
-</div>\n \
-<!-- -->\n \
-</div>\n \
-<div class=\"about-grid-right\">\n \
-<span class=\"glyphicon glyphicon-home\" aria-hidden=\"true\"></span>\n \
-</div>\n \
-<div class=\"clearfix\"> </div>\n \
-<!-- -->\n \
-<!-- 24062017i -->" $vendas
-
-sed -i "${inserir}d" $vendas
-#########
-
-fi
