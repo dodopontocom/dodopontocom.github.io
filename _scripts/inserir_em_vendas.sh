5286909e1a2816cba730f61ba80a342641bfc0f0
Neto <rodolfotiago@gmail.com>
2017-03-01 16:01:17 -0300

    inserir

--- _scripts/inserir_em_vendas.sh
+++ _scripts/inserir_em_vendas.sh
@@ -9,66 +9,54 @@ ref=$(cat $info | grep ref | cut -d':' -f2)
 eref=$(cat $info | grep eref | cut -d':' -f2)
 preco=$(cat $info | grep preco | cut -d':' -f2)
 metro=$(cat $info | grep metro | cut -d':' -f2)
-dormi=
-banho=
-titulo=
-descri=
-contato1=
-telefone1=
-contato2=
-telefone2=
-financiamento=
-item1=
-item2=
-item3=
-item4=
-item5=
-mconstru=
-destaque=
-locacao=
-captacao=
-prop=
-local=
-imagens=
+dormi=$(cat $info | grep dormi | cut -d':' -f2)
+banho=$(cat $info | grep banho | cut -d':' -f2)
+titulo=$(cat $info | grep titulo | cut -d':' -f2)
+descri=$(cat $info | grep descri | cut -d':' -f2)
+contato1=$(cat $info | grep contato1 | cut -d':' -f2)
+telefone1=$(cat $info | grep telefone1 | cut -d':' -f2)
+contato2=$(cat $info | grep contato2 | cut -d':' -f2)
+telefone2=$(cat $info | grep telefone2 | cut -d':' -f2)
+financiamento=$(cat $info | grep financiamento | cut -d':' -f2)
+item1=$(cat $info | grep item1 | cut -d':' -f2)
+item2=$(cat $info | grep item2 | cut -d':' -f2)
+item3=$(cat $info | grep item3 | cut -d':' -f2)
+item4=$(cat $info | grep item4 | cut -d':' -f2)
+item5=$(cat $info | grep item5 | cut -d':' -f2)
+mconstru=$(cat $info | grep mconstru | cut -d':' -f2)
+destaque=$(cat $info | grep destaque | cut -d':' -f2)
+locacao=$(cat $info | grep locacao | cut -d':' -f2)
+captacao=$(cat $info | grep captacao | cut -d':' -f2)
+prop=$(cat $info | grep prop | cut -d':' -f2)
+local=$(cat $info | grep local | cut -d':' -f2)
+imagens=$(cat $info | grep imagens | cut -d':' -f2)
 
 refnumber=$(cat $info | grep ref | cut -d':' -f2 | grep -o [0-9][0-9][0-9])
 
 #########
 sed -i "/24062017i/a \
-<div class="col-md-6 about-grid about-one">\n \
-<div class="about-grid-right aliquam">\n \
-<span class="glyphicon glyphicon glyphicon-home" aria-hidden="true"></span>\n \
-</div>\n \
-<div class="about-grid-left non">\n \
-<!-- -->\n \
+<!-- item imovel-->\n \
 <div class="w3l_services_grid">\n \
+<ul>\n \
+<li class="square">${metro} m²</li>\n \
+<li class="bath">${banho}</li>\n \
+<li class="bed">${dormi}</li>\n \
+</ul>\n \
 <div class="hover15 column">\n \
 <div>\n \
-<a href="ref002.html">\n \
+<a href="${ref}.html">\n \
 <figure>\n \
-<img src="../images/REF002/002.jpg" alt=" " class="img-responsive" />\n \
-<h5 class="cima"><span class="refspan">REF002</span></h5>\n \
-<h5 class="local"><span class="refspan">Porta do Sol - Mairinque</span></h5>\n \
+<img src="../images/REF${refnumber}/001.jpg" alt=" " class="img-responsive" />\n \
+<h5 class="cima"><span class="refspan">REF${refnumber}</span></h5>\n \
+<h5 class="local"><span class="refspan">${titulo}</span></h5>\n \
 </figure>\n \
 </a>\n \
 </div>\n \
 </div>\n \
-<h4><a href="ref002.html">R$ 790.000,00</a></h4>\n \
-<p><a class="bake" href="ref002.html">Detalhes...</a></p>\n \
-<ul>\n \
-<li class="square">3.000 m²</li>\n \
-<li class="bath">4</li>\n \
-<li class="bed">4</li>\n \
-</ul>\n \
+<h4><a href="${ref}.html">R$ ${preco}</a></h4>\n \
+<p>${descri}<a class="bake" href="${ref}.html">Detalhes...</a></p>\n \
 </div>\n \
-<!-- -->\n \
-</div>\n \
-<div class="clearfix"> </div>\n \
-</div>\n \
-</div>\n \
-</div>\n \
-<!-- finish other grid -->\n \
-<!-- -->\n \
+<p class="dolor"></p>\n \
 <!-- 24062017i -->" $vendas
 
 sed -i "${inserir}d" $vendas
