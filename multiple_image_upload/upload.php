0d2ef0e9e5a50a9770ecdcec62360a27c6ba8ad0
Neto <rodolfotiago@gmail.com>
2017-03-14 11:38:19 -0300

    php files

--- multiple_image_upload/upload.php
+++ multiple_image_upload/upload.php
@@ -8,7 +8,8 @@ if (isset($_POST['submit'])) {
         $validextensions = array("txt", "jpeg", "jpg", "png");  //Extensions which are allowed
         $ext = explode('.', basename($_FILES['file']['name'][$i]));//explode file name from dot(.) 
         $file_extension = end($ext); //store extensions in the variable
-        echo $_FILES['file']['type'];
+        /*echo $_FILES['file']['type'];
+        */
         
 		$target_path = $target_path . md5(uniqid()) . "." . $ext[count($ext) - 1];//set the target path with a new name of image
         $j = $j + 1;//increment the number of uploaded images according to the files in array       
