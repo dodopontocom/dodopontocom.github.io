2b460e3c8b638a6b00cd4cd167ce9c5f295d3f79
Rodolfo <alexnetoignacio@gmail.com>
2017-03-15 21:31:39 -0300

    hum... teste

--- _scripts/.verificar_se_houve_upload.sh
+++ _scripts/.verificar_se_houve_upload.sh
@@ -7,54 +7,55 @@ uploadfolder=${HOME}/public_html/multiple_image_upload/uploads
 bkpfolder=${HOME}/uploads_site
 newfolder=${bkpfolder}/pasta-${timestamp}
 
+
+#verifica se ha novos uploads
 if [ $(ls -1A ${uploadfolder} | wc -l) -gt 0 ]; then
 	mkdir ${newfolder}
 	mv ${uploadfolder}/* ${newfolder}/
 fi
 
-find ${bkpfolder}/pasta* \( -iname "*.jpeg" -or -iname "*.jpg" \) | while read -r FILE
-do
-	mv -v "$FILE" ${thisscript}/../images/REF${imovel}/${count}.jpg
-	count=$((count+1))
-done
+#cria nova pasta do anuncio no repositorio
 
+num=$(ls -1A ${HOME}/dodopontocom.github.io/refs/ref*.html | wc -l)
+sum=$((num+1))
+novo=REF0${sum}
+novolower=ref0${sum}
+novofullpath=${HOME}/dodopontocom.github.io/images/${novo}/
+echo "caminho do novo anuncio: $novofullpath"
 
-
-
-
-
-
-====================
-#!/bin/bash
-
-
-
-echo "script: $0"
-
-thisscript="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-timestamp=$(date -u "+%Y-%d-%m-%H-%MZ")
-
-#validando argumentos
-if [[ "$#" -eq 0 || "$#" -gt 1 ]]; then
-	echo "precisa de 1 e somente 1 argumento!"
-	exit -1
+if [ -d ${novofullpath} ]; then
+	echo "novo anuncio ja foi adicionado..."
+	exit 0
+else
+	mkdir ${novofullpath}
+	if [ "$?" -eq "0"]; then
+		echo "${novo} foi criado em ${novofullpath}"
+	else
+		echo "algo deu errado... saindo"
+		exit 0
+	fi
+	cp ${novofullpath}/../../info.txt ${novofullpath}
+	sed -i "s/^ref:/ref:${novolower}/g" ${novofullpath}/info.txt
 fi
-echo "argumento is $1"
 
-imovel=$1
+#########
 
+#mover imagens do bkpfolder para a pasta do anuncio atual
+#mudar os nomes das imagens
+
+mv ${newfolder}/* ${novofullpath}/
 
 count=1
 
-find ${thisscript}/../images/REF${imovel}/ \( -iname "*.jpeg" -or -iname "*.jpg" \) | while read -r FILE
+find ${novofullpath} \( -iname "*.jpeg" -or -iname "*.jpg" \) | while read -r FILE
 do
-	mv -v "$FILE" ${thisscript}/../images/REF${imovel}/${count}.jpg
+	mv -v "$FILE" ${novofullpath}/${count}.jpg
 	count=$((count+1))
 done
 
 
 
-reset
+
+
 
 exit 0
-
