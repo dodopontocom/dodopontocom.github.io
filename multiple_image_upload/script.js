e700dd56ae532685cf48b41469cfa10b59691e91
Rodolfo <rodolfotiago@gmail.com>
2017-03-17 20:44:14 -0300

    só habilita botao submeter se todos os obrigatorios estiverem preenchidos

--- multiple_image_upload/script.js
+++ multiple_image_upload/script.js
@@ -2,6 +2,9 @@ var abc = 0; //Declaring and defining global increement variable
 
 $(document).ready(function() {
 
+
+
+
 //To add new input file field dynamically, on click of "Add More Files" button below function will be executed
     $('#add_more').click(function() {
         $(this).before($("<div/>", {id: 'filediv'}).fadeIn('slow').append(
