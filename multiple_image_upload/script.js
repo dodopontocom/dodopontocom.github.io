d41ba2a69882660e944ae3278dbfbfba6cf178d8
Neto <rodolfotiago@gmail.com>
2017-03-17 17:23:22 -0300

    testando required field

--- multiple_image_upload/script.js
+++ multiple_image_upload/script.js
@@ -2,8 +2,6 @@ var abc = 0; //Declaring and defining global increement variable
 
 $(document).ready(function() {
 
-
-
 //To add new input file field dynamically, on click of "Add More Files" button below function will be executed
     $('#add_more').click(function() {
         $(this).before($("<div/>", {id: 'filediv'}).fadeIn('slow').append(
@@ -61,7 +59,7 @@ $('body').on('change', '#file', function(){
             });
 
             swal("Ótimo!","Música correta!", "success");
-            
+
         }else {
             swal("Oops...","Música inválida!", "error");
             e.preventDefault();
