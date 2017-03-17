d935696d225e08fc9e259e020b76e13ae3d7e094
Rodolfo <alexnetoignacio@gmail.com>
2017-03-16 21:59:15 -0300

    php

--- multiple_image_upload/upload.php
+++ multiple_image_upload/upload.php
@@ -19,16 +19,16 @@ if (isset($_POST['submit'])) {
 	  if (($_FILES["file"]["size"][$i] < 10000000) //Approx. 10000kb files can be uploaded.
                 && in_array($file_extension, $validextensions)) {
             if (move_uploaded_file($_FILES['file']['tmp_name'][$i], $target_path)) {//if file moved to uploads folder
-                echo $j. ').<span id="noerror">Image uploaded successfully!.</span><br/><br/>';
+                echo $j. ').<span id="noerror">Imagem enviada com sucesso!.</span><br/><br/>';
             } else {//if file was not moved.
-                echo $j. ').<span id="error">please try again!.</span><br/><br/>';
+                echo $j. ').<span id="error">Tente novamente!.</span><br/><br/>';
             }
         } else {//if file size and file type was incorrect.
-            echo $j. ').<span id="error">***Invalid file Size or Type***</span><br/><br/>';
+            echo $j. ').<span id="error">***O tipo ou tamanho das imagens são inválidos***</span><br/><br/>';
         }
     }
 
-    echo $_POST["preco"];
+    
 
     $infotxt = fopen("uploads/newinfo.txt", w) or die("não conseguiu criar arquivo!");
 
@@ -106,5 +106,29 @@ if (isset($_POST['submit'])) {
 
     fwrite($infotxt, "\n");
     fclose($infotxt);
+
+    //mostrar o que foi enviado com sucesso
+    echo $_POST["preco"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["metro"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["dormi"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["banho"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["titulo"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["descri"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["contato1"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["telefone1"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["contato2"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["telefone2"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["financiamento"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["item1"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["item2"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["item3"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["item4"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["item5"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["item6"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["destaque"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["locacao"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["captacao"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["prop"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
+    echo $_POST["local"] . ').<span id="noerror">Informação enviada com sucesso!.</span><br/><br/>';
 }
 ?>
