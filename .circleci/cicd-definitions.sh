b357ca594b6817c6ceeb3e6abf1b4d88a57dd12c
rodolfotiago <rodolfotiago@gmail.com>
2020-04-22 09:52:29 -0300

    test in develop branch

--- .circleci/cicd-definitions.sh
+++ .circleci/cicd-definitions.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Gcloud project id
+# GCLOUD ENV VARS
 export GCLOUD_PROJECT_ID="construtora-2020"
-
-# GCLOUD service account credentials path
-export GCLOUD_JSON_KEY_PATH="${CIRCLE_WORKING_DIRECTORY}/credentials/credential.json"
+export GCLOUD_JSON_KEY_PATH="${CIRCLE_WORKING_DIRECTORY}/cloud/credentials/credential.json"
 export GOOGLE_APPLICATION_CREDENTIALS=${GCLOUD_JSON_KEY_PATH}
-#export GCLOUD_PROJECT_BUCKET_NAME="www.construtoracarvalhopereira.com.br"
-export GCLOUD_PROJECT_BUCKET_NAME="website_github_sync"
+export GCLOUD_PROJECT_BUCKET_NAME="www.construtoracarvalhopereira.com.br"
 export GCLOUD_PROJECT_REGION="southamerica-east1"
