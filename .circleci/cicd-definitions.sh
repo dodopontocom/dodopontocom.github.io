9685342cdefd1a122fcde3f5e9561ccaa243e02d
Rodolfo Neto <rodolfotiago@gmail.com>
2020-04-21 22:35:14 -0300

    Create cicd-definitions.sh

---
+++ .circleci/cicd-definitions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Gcloud project id
+export GCLOUD_PROJECT_ID="construtora-2020"
+
+# GCLOUD service account credentials path
+export GOOGLE_APPLICATION_CREDENTIALS=${GCLOUD_JSON_KEY_PATH}
+export GCLOUD_JSON_KEY_PATH="${CIRCLE_WORKING_DIRECTORY}/credentials/credential.json"
+export GCLOUD_PROJECT_BUCKET_NAME="www.construtoracarvalhopereira.com.br"
+export GCLOUD_PROJECT_REGION="southamerica-east1"
